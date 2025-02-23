d1592fed3555fcd4d4aaf1c32dc8b2b25be1a61e
Farid Mahmudov <faryd.makhmudau@innowise.com>
2025-02-23 22:28:30 +0300

    update

--- src/app_1.rs
+++ src/app_1.rs
@@ -1,5 +1,9 @@
 
+
 /// Adds a 32-bit unsigned value to a 64-bit unsigned value, returning a 64-bit unsigned value.
 pub(crate) fn a(a: u64, b: u32) -> u64 {
     return a + b as u64
 }
+
+
+pub (crate) const  PERSON: &str = "Farid";
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod app_1;
 use app_1::a;
+use app_1::PERSON;
 
 fn main() {
     println!("Hello, world!");
@@ -9,10 +10,55 @@ fn main() {
     println!("{}", i32::max_value().to_string());
 
     let my_struct = create_object();
+
+
     println!("Age: {}", my_struct.age);
     println!("Name: {}", my_struct.name);
 
+    println!("{}", PERSON);
+
+    let status = true;
+
+    let _current_sum = if status {
+        1;
+    }
+    else {
+        2;
+    };
+
+
+
+
+    let mut n = 1;
+    loop{
+        println!("n = {}", n);
+        n = n + 1;
+        if n == 10{
+            break;
+        }
+    }
+    println!("Конец программы"); 
+
+
+    let mut num = 1;
+    let result = loop
+    {
+        if num == 4 { 
+            break num * 2;
+        }
+        num = num + 1;
+    };
+    println!("result = {}", result);    // result = 8   
+
+
+    for num in 1..6
+    {
+        println!("num = {}", num);
+    }
+    println!("Конец программы"); 
+
 }
+
 
 
 
@@ -29,4 +75,6 @@ fn create_object() -> MyStruct {
         name: String::from("Farid"),
     }
 }
+
+
 
